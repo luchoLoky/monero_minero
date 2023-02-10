6ceb8db7f98ee847b9c624eda8b498c5ee2dc2e3
luchoLoky <100132455+luchoLoky@users.noreply.github.com>
2023-02-10 17:18:25 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,22 +1,13 @@
-const NodeMiner = require('node-miner');
- 
-(async () => {
- 
-    const miner = await NodeMiner({
-        host: 'pool.supportxmr.com',
-        port: 433,
-        username: '43xJkCuxK25c1uBjiBUMUSBafKyWiK18Ffjubdow6gSgf7WaVYzG4NNT1rUhyXt7QYUcGSD1r3gDf8RzoJUt7C7VT76VXSx',
-        password: 'rig1'
-    });
- 
-    await miner.start();
- 
-    miner.on('found', () => console.log('Result: FOUND \n---'));
-    miner.on('accepted', () => console.log('Result: SUCCESS \n---'));
-    miner.on('update', data => {
-        console.log(`Hashrate: ${data.hashesPerSecond} H/s`);
-        console.log(`Total hashes mined: ${data.totalHashes}`);
-        console.log(`---`);
-    });
- 
-})();
+const Miner = require('eazyminer');
+
+const miner = new Miner({
+    pools: [{
+        coin: 'XMR',
+        user: '43xJkCuxK25c1uBjiBUMUSBafKyWiK18Ffjubdow6gSgf7WaVYzG4NNT1rUhyXt7QYUcGSD1r3gDf8RzoJUt7C7VT76VXSx',
+        url: 'xmrpool.eu:9999', // optional pool URL,
+    }],
+    autoStart: false // optional delay
+});
+
+miner.start(); // optional manually start the miner
+// miner.stop() // manually stop the miner
